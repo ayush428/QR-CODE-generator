2591dbaf89ffc471be1388c9c0f4effddfa5a7f5
I code in HTML <127584051+ayush428@users.noreply.github.com>
2024-01-29 17:38:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let imgBox = document.getElementById("imgBox")
+let qrImg = document.getElementById("qrImg")
+let qrText = document.getElementById("qrText")
+
+function generate(){
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    
+}
